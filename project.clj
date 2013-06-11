c2672569a687df5f6dc7f28242e3ec1c12207b8a
Ray Miller <ray@1729.org.uk>
2013-06-11 21:40:44 +0100

    Fix project URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.ddduk/util.cap-seq-planning "0.1.0-SNAPSHOT"
   :description "Brute-force optimization example"
-  :url "https://github.com/ray1729/cap-seq-planning"
+  :url "https://github.com/ray1729/CapSeqPlanning"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
